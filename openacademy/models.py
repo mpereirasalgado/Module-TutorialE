f1f32f6a394f81793fc93b33753e6d6901e6c14f
Micael <micael11rm@ourlook.com>
2016-03-29 19:14:10 +0200

    Noveno commit: cambiamos el treeview  de las sesiones para diferenciar las sessiones por colores segun la duracion de la sesion menos de 5 dias azul entre 5 y 15 negro y mas de 15 rojo

--- openacademy/models.py
+++ openacademy/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models, fields, api, exceptions
+
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -10,9 +11,37 @@ class Course(models.Model):
     
     responsible_id = fields.Many2one('res.users',
         ondelete='set null', string="Responsible", index=True)
-        
+
     session_ids = fields.One2many(
         'openacademy.session', 'course_id', string="Sessions")
+        
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
+        
+    
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "The title of the course should not be the description"),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
+
+
 
 
 class Session(models.Model):
@@ -32,6 +61,7 @@ class Session(models.Model):
         ondelete='cascade', string="Course", required=True)
 
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
     
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
 

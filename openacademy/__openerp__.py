2810d041ee70be0a41bddf6ba4e73cbd4cc3e0e1
Micael <micael11rm@ourlook.com>
2016-03-29 13:04:35 +0200

    Cuarto commit: creacion de partner.py creacion de partner.xml modificacion de archivos para crear la pestaña partners(Contactos ) y asi poder modificarlos

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -27,7 +27,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'templates.xml',
-        'openacademy.xml',
+        'openacademy.xml','partner.xml'
     ],
 
     # only loaded in demonstration mode

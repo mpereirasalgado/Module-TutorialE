40094ddf54d9270bd5da2d928aa1a5a031517326
Micael <micael11rm@ourlook.com>
2016-03-29 11:47:19 +0200

    Tercero commit: modificacion de archivos para crear relaciones many2many y añadir attendees datos de los asistentes a las sesiones

--- openacademy/models.py
+++ openacademy/models.py
@@ -27,4 +27,7 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
 
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+
 
